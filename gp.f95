3db4be3ca2f4693cc9b3304f6f4f77db20a62fa3
George Stagg <a8034837@heartofgold.(none)>
2013-03-26 00:52:56 +0000

    updates to vortex detection code
    l

--- gp.f95
+++ gp.f95
@@ -3,29 +3,29 @@ program gp
 	
 	implicit none
 	character(len=80) fname
-	
+	double precision :: ret
+
 	do LOOPNO = VOBS,VOBE,VOBST
 			!Initialise
 			GRID = 0
 			TIME = 0.0d0
+			VOB = DBLE(LOOPNO)/100.0d0
 			DT = (0,-0.01d0)
-			!!!!!!!!!!!!!
-
-			write(fname, '(a,i0)') 'utils.',LOOPNO
-			open (8, FILE = fname)
 			call calc_OBJPOT(1)
 			call approx
+			write(fname, '(a,i0)') 'utils.',LOOPNO
+			open (8, FILE = fname)
+			!!!!!!!!!!!!
 			call runit(2000,0,0)
-			VOB=LOOPNO/100.0d0
 			DT = (0.01d0,0)
-			call add_noise
+			!call add_noise
 			call runit(NSTEPS,1,1)
+			close(8)
 	end do
 end PROGRAM gp
 
 subroutine runit(steps,rt,plot)	
 	use params
-	
 	implicit none
 	integer :: steps,rt,i,plot
 	do i = 1, steps
@@ -50,9 +50,11 @@ subroutine runit(steps,rt,plot)
 				end if	
 			end if
 		end if
-		if (modulo(i,1000) == 0) then
-			call dump_density(i)
-			!call dump_wavefunction(i)
+		if (modulo(i,100) == 0) then
+			if (plot == 1) then
+				call dump_density(i)
+				!call dump_wavefunction(i)
+			end if
 		end if
 	end do
 end subroutine
@@ -61,186 +63,60 @@ end subroutine
 
 subroutine dump_density (II)
 	use params
-	
 	implicit none
-	double precision :: small
-	integer :: II,i,j,vort=0
+	integer :: II,i,j
 	character(len=80) fname
-	complex*16 :: u,uux,uuy,uu,cuux,cuuy
 	double precision, dimension(-NX/2:NX/2,-NY/2:NY/2) :: phase,velx,vely
-	FORALL(i = -NX/2:NX/2, j = -NY/2:NY/2)
-	phase(i,j) = ATAN(AIMAG(GRID(i,j))/dble(GRID(i,j)+TINY(0.0d0)))
-	end FORALL
+	call calc_phase(phase)
 	call velxy(phase,velx,vely)
 	write(fname, '(i0,a,i0.4)') LOOPNO,'plot.',II/100
 	open (7, FILE = fname)
 	do i = -NX/2, NX/2
 		do j = -NY/2, NY/2
 			write (unit=7,fmt="(f10.2,f10.2,5f15.10)")&
-			dble(i),dble(j),dble(GRID(i,j)*CONJG(GRID(i,j))),&
+			dble(i*DSPACE),dble(j*DSPACE),dble(GRID(i,j)*CONJG(GRID(i,j))),&
 			 phase(i,j),SQRT((velx(i,j)**2.0d0)+(vely(i,j)**2.0d0)),velx(i,j),vely(i,j)
 		end do
 		write (unit=7,fmt="(a)") " "
 	end do
 	close(7)
+end subroutine
 
+subroutine dump_wavefunction (II)
+	use params
+	implicit none
+	integer :: II,i,j
+	character(len=80) fname
+	write(fname, '(i0,a,i0.4)') LOOPNO,'plot.',II/100
+	open (7, FILE = fname)
+	do i = -NX/2, NX/2
+		do j = -NY/2, NY/2
+			write (unit=7,fmt="(f10.2,f10.2,5f15.10)")&
+			dble(i*DSPACE),dble(j*DSPACE),dble(GRID(i,j)),aimag(GRID(i,j))
+		end do
+		write (unit=7,fmt="(a)") " "
+	end do
+	close(7)
 end subroutine
 
 subroutine dump_vortex_locations (II)
 	use params
 	
 	implicit none
-	double precision :: ret
-	integer :: II,ti,tj,i,j,k,vort
+	integer :: II,k,vort
 	character(len=80) fname
-	complex*16 :: u,uux,uuy,uu,cuux,cuuy
-	double precision, dimension(-NX/2:NX/2,-NY/2:NY/2) :: vorticity
 	double precision, dimension(0:NX*NY,0:2) :: vortarray
 	vort=0
 	vortarray=0.0d0
-	vorticity=0.0d0
-	!call GETVORTICES(vort,vortarray)	
+	call get_vortices(vort,vortarray)	
 	write(fname, '(i0,a,i0.4)') LOOPNO,'plotvort.',II/100
 	open (7, FILE = fname)
 	do k=0,vort-1
-	if(vortarray(k,2) > 0) then
-		write (unit=7,fmt="(f10.2,f10.2,i5)") vortarray(k,0),vortarray(k,1),0
-	else
-		write (unit=7,fmt="(f10.2,f10.2,i5)"),vortarray(k,0),vortarray(k,1),1
-	end if
+		if(vortarray(k,2) > 0) then
+			write (unit=7,fmt="(f10.2,f10.2,i5)") vortarray(k,0)*DSPACE,vortarray(k,1)*DSPACE,0
+		else
+			write (unit=7,fmt="(f10.2,f10.2,i5)"),vortarray(k,0)*DSPACE,vortarray(k,1)*DSPACE,1
+		end if
 	end do
 	close(7)
-
-end subroutine
-
-
-subroutine LINEINTVF(fieldx,fieldy,x,ex,y,ey,ret)
-use params
-	implicit none
-	integer :: t,x,ex,y,ey
-	double precision, dimension(-NX/2:NX/2,-NY/2:NY/2) :: fieldx,fieldy
-	double precision l1,l2,l3,l4,ret
-	l1=0.0d0
-	l2=0.0d0
-	l3=0.0d0
-	l4=0.0d0
-	do t = y,ey
-		l1 = l1 + DSPACE*fieldy(x,t)
-	end do
-	do t = x,ex
-		l2 = l2 + DSPACE*fieldx(t,y)
-	end do
-	do t = y,ey
-		l3 = l3 + DSPACE*fieldy(ex,t)
-	end do
-	do t = x,ex
-		l4 = l4 + DSPACE*fieldx(t,ey)
-	end do
-	ret = l2+l3-l4-l1
-end subroutine
-
-subroutine velxy(phase,velx,vely)
-use params
-	implicit none
-	integer :: i,j
-	double precision, dimension(-NX/2:NX/2,-NY/2:NY/2) :: phase,velx,vely
-	double precision :: temp1,temp2
-	velx = 0.0d0
-	vely = 0.0d0
-	do i = -NX/2+1,NX/2-1
-	do j = -NY/2+1,NY/2-1
-		if (phase(i+1,j)-phase(i-1,j)<-(pi/2.0d0)) then
-			temp1 = phase(i+1,j)-(phase(i-1,j) - PI)
-		else if (phase(i+1,j)-phase(i-1,j)>(pi/2.0d0)) then
-			temp1 = phase(i+1,j)-(phase(i-1,j) + PI)
-		else
-			temp1 = phase(i+1,j)-phase(i-1,j)
-		end if
-		velx(i,j) = temp1
-	end do
-	end do
-	
-	do i = -NX/2+1,NX/2-1
-	do j = -NY/2+1,NY/2-1
-		if (phase(i,j+1)-phase(i,j-1)<-(pi/2.0d0)) then
-			temp1 = phase(i,j+1)-(phase(i,j-1) - PI)
-		else if (phase(i,j+1)-phase(i,j-1)>(pi/2.0d0)) then
-			temp1 = phase(i,j+1)-(phase(i,j-1) + PI)
-		else
-			temp1 = phase(i,j+1)-phase(i,j-1)
-		end if
-		vely(i,j) = temp1
-	end do
-	end do
-end subroutine
-
-subroutine GETVORTICES(vort,vortarray)
-        use params
-        
-        implicit none
-        integer :: i,j,k,vort,dvort,vlength,np,nn
-        double precision, dimension(-NX/2:NX/2,-NY/2:NY/2) :: vela
-        complex*16 :: uux,uuy,uu,eng
-        double precision :: dist,temp,psi,ret
-        double precision, dimension(0:NX*NY,0:2) :: vtrack,vortarray
-        double precision, dimension(-NX/2:NX/2,-NY/2:NY/2) :: phase,velx,vely
-        vtrack = 0.0d0
-        vlength = 0
-        np=0
-        nn=0
-		FORALL(i = -NX/2:NX/2, j = -NY/2:NY/2)
-			phase(i,j) = ATAN(AIMAG(GRID(i,j))/dble(GRID(i,j)+TINY(0.0d0)))
-		end FORALL
-	
-		call velxy(phase,velx,vely)
-        do i = -NX/2+10, NX/2-10
-        do j = -NY/2+10, NY/2-10
-                dist = 99999999.0d0
-				uu=GRID(i,j)
-				uux=(GRID(i+1,j)-GRID(i-1,j))/(2.0d0*DSPACE)
-				uuy=(GRID(i,j+1)-GRID(i,j-1))/(2.0d0*DSPACE)
-                eng = 0.5d0*(uux+uuy)*conjg(uux+uuy) &
-				+ 0.5d0*((uu*conjg(uu)-1.0d0)**2.0d0)&
-				+ OBJPOT(i,j)*uu*conjg(uu)
-				psi = dble(GRID(i,j)*CONJG(GRID(i,j)))
-			
-                if (dble(OBJPOT(i,j))<1.0d0 .and. dble(psi)<0.2d0 .and. SQRT((velx(i,j)**2.0d0)+(vely(i,j)**2.0d0))>1.2d0) then
-                        !FOUND ONE AT i, j
-                        if(vlength > 0) then
-                                do k = 0, vlength-1
-                                        temp = SQRT((dble(i-vtrack(k,0))**2.0d0) + (dble(j-vtrack(k,1))**2.0d0))
-                                        !ITS DISTANCE FROM EXISTSING POINT k IS temp
-                                        if(temp < 6.0d0) then
-                                        !AVERAGING vtrack(k,0),vtrack(k,1) and i,j
-                                  		!write (unit=6,fmt="(a, 2f10.2, a , 2i5)") "Averaging ", vtrack(k,0),vtrack(k,1), " and ", i,j
-                                        	vtrack(k,0) = (vtrack(k,0)+dble(i))/2.0d0
-                                    	    vtrack(k,1) = (vtrack(k,1)+dble(j))/2.0d0
-                                    	!write (unit=6,fmt="(a, 2f10.2)") "We get ", vtrack(k,0),vtrack(k,1)
-                                    	end if
-                                        if(temp < dist) then
-                                        dist = temp
-                                        end if
-                                end do
-                                if(dist > 6.0d0 ) then
-                                        vtrack(vlength,0) = dble(i)
-                                        vtrack(vlength,1) = dble(j)
-                                        call LINEINTVF(velx,vely,i-5,i+5,j-5,j+5,ret)
-                             			vtrack(vlength,2) = ret
-                                        vlength = vlength + 1
-                                      	!write (unit=6,fmt="(i3, a, i3, a, f10.7)") i, " , ", j, " : ", uux
-                                end if
-                        else
-                                vtrack(vlength,0) = dble(i)
-                                vtrack(vlength,1) = dble(j)
-                                call LINEINTVF(velx,vely,i-5,i+5,j-5,j+5,ret)
-                                vtrack(vlength,2) = ret
-                                vlength = vlength + 1
-                                !write (unit=6,fmt="(i3, a, i3, a, f10.7)") i, " , ", j, " : ", uux
-                        end if
-                end if
-        end do
-        end do
-        vort =  vlength
-		vortarray = vtrack
-        !write (unit=6,fmt="(i5)") vlength
 end subroutine
--- makemovie.sh
+++ makemovie.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-for count in {0080..3000};
-echo "running gnuplot - $count
+echo "running gnuplot
 "
+for count in {0001..0300};
 do
 if [ "$2" = "density" ]; then
 echo "
 set pm3d map
 set title ''
-set terminal png enhanced size 2048,512 crop;
+set terminal png enhanced size 512,512 crop;
 set cbrange [0:$3]
 set output '$count.png';
 splot '$1plot.$count' using 1:2:3" | gnuplot;
@@ -17,8 +17,8 @@ if [ "$2" = "phase" ]; then
 echo "
 set pm3d map
 set title ''
-set terminal png enhanced size 2048,600  crop;
-set cbrange [-5:5];
+set terminal png enhanced size 512,512  crop;
+set cbrange [-1.57:1.57];
 set output '$count.png';
 splot '$1plot.$count' using 1:2:4" | gnuplot;
 fi
@@ -26,35 +26,27 @@ if [ "$2" = "vort" ]; then
 echo "
 set title '';
 unset key;
-set terminal png enhanced size 720,320 crop font arial 20;
+set terminal png enhanced size 1024,1024 crop;
 set output '44_$count.png';
-set xrange [220:400];
-set yrange [-40:40];
-set object 1 ellipse center 980*0.4,0 size 3.0*2*2,3.0*2*2*3 angle 0. front fillstyle solid 1.0 fc rgb 'black'
+set xrange [-60:60];
+set yrange [-60:60];
+set object 1 ellipse center 0,0 size 1.5,2*1.5 angle 0. front fillstyle solid 1.0 fc rgb 'black'
 set style line 9 linecolor rgb 'blue' pt 6 ps 1
 set style line 7 linecolor rgb 'red' pt 7 ps 1
-plot '$1plotvort.$count' u (\$3==0?-1000:\$1*0.4):(\$3==0?-1000:\$2*0.4) ps 1 pt 7 lc 3,'$1plotvort.$count' u (\$3==1?-1000:\$1*0.4):(\$3==1?-1000:\$2*0.4) ps 1 pt 9 lc 1" | gnuplot;
+plot '$1plotvort.$count' u (\$3==0?-1000:\$1):(\$3==0?-1000:\$2) ps 1 pt 7 lc 3,'$1plotvort.$count' u (\$3==1?-1000:\$1):(\$3==1?-1000:\$2) ps 1 pt 9 lc 1" | gnuplot;
 fi
-done
-
-
-echo "converting png images to jpg
-"
-for i in `ls -1| grep png`;
-do
-convert $i $i.jpg;
 done
 
 echo "creating movie $2.avi
 "
 if [ "$2" = "density" ]; then
-mencoder "mf://*.jpg" -mf fps=3 -ovc lavc -lavcopts vcodec=msmpeg4v2:vbitrate=8000 -o density.avi 
+mencoder "mf://*.png" -mf fps=30 -ovc lavc -lavcopts vcodec=msmpeg4v2:vbitrate=20971520 -o density.avi 
 fi
 if [ "$2" = "phase" ]; then
-mencoder "mf://*.jpg" -mf fps=30 -ovc xvid -ovc lavc  -o phase.avi
+mencoder "mf://*.png" -mf fps=30 -ovc xvid -ovc lavc  -o phase.avi
 fi
 if [ "$2" = "vort" ]; then
-mencoder "mf://*.jpg" -mf fps=30 -ovc lavc -lavcopts vcodec=msmpeg4v2:vbitrate=8000 -o vort.avi 
+mencoder "mf://*.png" -mf fps=30 -ovc lavc -lavcopts vcodec=msmpeg4v2:vbitrate=20971520 -o vort.avi 
 fi
 rm -Rf *.jpg *.png
 
