f0bfc3a9799a57f9ce18a8fed7fa660cfed411ff
a8034837 <george.stagg@ncl.ac.uk>
2014-03-18 13:29:59 +0000

    new version

--- params.f95
+++ params.f95
@@ -1,7 +1,7 @@
 module params
-integer,parameter :: NX = 256,NY=256
-double precision :: DSPACE = 0.4d0,VOB,EPS=2.0d0,R1=1.5d0
-integer :: VOBS = 80, VOBE = 80, VOBST = 4,LOOPNO,OBJXDASH=0,NSTEPS=30000
+integer,parameter :: NX = 1024,NY=512
+double precision :: DSPACE = 0.4d0,VOB,RRX=6.0d0,RRY=6.0d0
+integer :: VOBS = 35, VOBE = 35, VOBST = 1,LOOPNO=5,OBJXDASH=0,NSTEPS=300000
 double precision :: PI = 4.0d0*ATAN(1.0d0)
 complex*16 :: DT,EYE = (0.0d0,1.0d0)
 
