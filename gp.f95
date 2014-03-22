cdcd93cd579846982f784e5124dfffc0ac3f1ea8
U-RedDwarf\George <George@RedDwarf.(none)>
2014-03-22 12:08:45 +0000

    move vortex code to matlab

--- gp.f95
+++ gp.f95
@@ -48,13 +48,6 @@ subroutine runit(steps,rt,plot)
 			end if
 			close(10)
 		end if
-		if (modulo(i,dumpv) == 0) then
-			if (rt == 1) then
-				if (plot == 1) then	
-					call dump_vortex_locations (i)
-				end if	
-			end if
-		end if
 		if (modulo(i,dumpd) == 0) then
 			if (rt == 1) then
 				if (plot == 1) then	
@@ -108,28 +101,6 @@ subroutine dump_wavefunction (II)
 			dble(i*DSPACE),dble(j*DSPACE),dble(GRID(i,j)),aimag(GRID(i,j)),DBLE(OBJPOT(i,j))
 		end do
 		write (unit=7,fmt="(a)") " "
-	end do
-	close(7)
-end subroutine
-
-subroutine dump_vortex_locations (II)
-	use params
-	
-	implicit none
-	integer :: II,k,vort
-	character(len=80) fname
-	double precision, dimension(0:NX*NY,0:2) :: vortarray
-	vort=0
-	vortarray=0.0d0
-	call get_vortices(vort,vortarray)	
-	write(fname, '(i0,a,i0.4)') LOOPNO,'plotvort.',II/dumpv
-	open (7, FILE = fname)
-	do k=0,vort-1
-		if(vortarray(k,2) > 0) then
-			write (unit=7,fmt="(f10.2,f10.2,i5)") vortarray(k,0)*DSPACE,vortarray(k,1)*DSPACE,0
-		else
-			write (unit=7,fmt="(f10.2,f10.2,i5)"),vortarray(k,0)*DSPACE,vortarray(k,1)*DSPACE,1
-		end if
 	end do
 	close(7)
 end subroutine
